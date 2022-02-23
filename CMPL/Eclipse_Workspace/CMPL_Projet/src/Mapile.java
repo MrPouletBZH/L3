7ab01e467cdf5d96ed8d7db3b288ceb9cb978807
MrPouletBZH <77390126+MrPouletBZH@users.noreply.github.com>
2022-02-23 17:13:05 +0100

    uhuh

--- CMPL/Eclipse_Workspace/CMPL_Projet/src/Mapile.java
+++ CMPL/Eclipse_Workspace/CMPL_Projet/src/Mapile.java
@@ -11,7 +11,7 @@ public class Mapile  {
     // necessite les classes ExecMapile.class (dans libClass_projet) et Mnemo.class 
     
     public static void main (String[] args) {
-    	ExecMapile.activer();
+    	ExecMapile.activer(); //LABITE
     }
     
 } // Mapile
